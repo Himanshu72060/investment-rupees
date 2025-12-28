a8c28950b2ece4ae94ed89279866b05f5b766659
Himanshu72060 <himanshusharma00898@gmail.com>
2025-12-28 15:33:40 +0530

    fix ssl mongo render

--- config/db.js
+++ config/db.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
+
+    console.log("MongoDB Connected Successfully");
   } catch (error) {
-    console.error("MongoDB Error:", error.message);
-    throw error;
+    console.error("MongoDB SSL Error:", error.message);
+    process.exit(1);
   }
 };
 
