21f106aeeddb510f5c966a8b9ce791e9324dda90
Himanshu72060 <himanshusharma00898@gmail.com>
2026-01-01 15:27:19 +0530

    fix admin route 404

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 
 
 const authRoutes = require("./routes/auth.routes");
-const userRoutes = require("./routes/user.routes");
 const investmentRoutes = require("./routes/investment.routes");
 const withdrawalRoutes = require("./routes/withdrawal.routes");
 const adminRoutes = require("./routes/admin.routes");
@@ -16,7 +15,6 @@ app.use(express.json());
 
 
 app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
 app.use("/api/invest", investmentRoutes);
 app.use("/api/withdraw", withdrawalRoutes);
 app.use("/api/admin", adminRoutes);
--- routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router(),
-    userController = require("../controllers/user.controller");
-
-router.post("/signup", userController.signup);
-router.post("/login", userController.login);
-
-module.exports = router;
