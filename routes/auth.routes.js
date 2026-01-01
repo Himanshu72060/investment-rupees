9e5e79367d8c7fa136e61808fddc33f5b2df3609
Himanshu72060 <himanshusharma00898@gmail.com>
2026-01-01 15:20:40 +0530

    fix admin route 404

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,8 +5,6 @@ const { signup, login } = require("../controllers/auth.controller");
 router.post("/signup", signup);
 router.post("/login", login);
 
-router.get("/profile", (req, res) => {
-    res.json({ message: "This is a profile route" });
-});
+router.get("/profile", getProfile);
 
 module.exports = router;
