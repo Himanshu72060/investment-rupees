e0b83ca14f5a338f0c282c4ae8021d2a717b2e03
Himanshu72060 <himanshusharma00898@gmail.com>
2026-01-01 15:19:46 +0530

    fix admin route 404

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -120,3 +120,28 @@ exports.login = async (req, res) => {
         });
     }
 };
+
+//  user get profile
+exports.getProfile = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const user = await User.findById(userId).select("-password");
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+        res.json({
+            success: true,
+            user
+        });
+    } catch (error) {
+        console.error("Get Profile Error:", error);
+        res.status(500).json({  
+            success: false,
+            message: error.message
+        });
+    }
+};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,4 +5,8 @@ const { signup, login } = require("../controllers/auth.controller");
 router.post("/signup", signup);
 router.post("/login", login);
 
+router.get("/profile", (req, res) => {
+    res.json({ message: "This is a profile route" });
+});
+
 module.exports = router;
