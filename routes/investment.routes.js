552120545738f9c4f05ce7aa918b5885de306846
Himanshu72060 <himanshusharma00898@gmail.com>
2026-01-04 16:24:54 +0530

    fix admin route 404

--- routes/investment.routes.js
+++ routes/investment.routes.js
@@ -2,23 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    createInvestment,
-    getMyInvestments,
-    getInvestmentSummary,
-    createDeposit
+  createDeposit,
+  getMyInvestments,
+  getInvestmentSummary
 } = require("../controllers/investment.controller");
 
 const authMiddleware = require("../middleware/auth.middleware");
 
 /*
-  POST  /api/investment/create
+  POST  /api/investment/deposit
   GET   /api/investment/my
   GET   /api/investment/summary
 */
 
-router.post("/create", authMiddleware, createInvestment);
+router.post("/deposit", authMiddleware, createDeposit);
 router.get("/my", authMiddleware, getMyInvestments);
 router.get("/summary", authMiddleware, getInvestmentSummary);
-router.post("/deposit", authMiddleware, createDeposit);
 
 module.exports = router;
