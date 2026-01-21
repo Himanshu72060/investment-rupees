265c91f72e44760b7f3a67c7b9844f3e46dc27b5
Himanshu72060 <himanshusharma00898@gmail.com>
2026-01-21 19:09:17 +0530

    fix admin route 404

--- routes/admin/deposit.admin.routes.js
+++ routes/admin/deposit.admin.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const {
     approveDeposit,
+    updateDepositStatus,
+    rejectDeposit,
     getAllDeposits
 } = require("../../controllers/admin/deposit.admin.controller");
 
@@ -10,6 +12,8 @@ const adminMiddleware = require("../../middleware/admin.middleware");
 
 // ✅ ADMIN APIs
 router.get("/deposit/deposits", adminMiddleware, getAllDeposits);
+router.put("/deposit/update-status/:id", adminMiddleware, updateDepositStatus);
+router.put("/deposit/reject/:id", adminMiddleware, rejectDeposit);
 router.put("/deposit/approve/:id", adminMiddleware, approveDeposit);
 
 module.exports = router;
