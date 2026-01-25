8db2741f7716b23b0efd3b5daeb9de29921d6867
Himanshu72060 <himanshusharma00898@gmail.com>
2026-01-25 11:07:47 +0530

    fix admin route 404

--- controllers/withdrawal.controller.js
+++ controllers/withdrawal.controller.js
@@ -89,3 +89,60 @@ exports.getMyWithdrawals = async (req, res) => {
         });
     }
 };
+
+/* =========================
+update WITHDRAW STATUS
+========================= */
+exports.updateWithdrawalStatus = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { status } = req.body;
+        const validStatuses = ["pending", "approved", "rejected", "withdraw"];
+
+        if (!validStatuses.includes(status)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid status value"
+            });
+        }
+        const withdrawal = await Withdrawal.findById(id);
+        if (!withdrawal) {
+            return res.status(404).json({
+                success: false,
+                message: "Withdrawal request not found"
+            });
+        }
+        withdrawal.status = status;
+        await withdrawal.save();
+        res.json({
+            success: true,
+            message: "Withdrawal status updated",
+            withdrawal
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+/* =========================
+   ALL WITHDRAWALS (ADMIN)
+========================= */
+exports.getAllWithdrawals = async (req, res) => {
+    try {
+        const withdrawals = await Withdrawal.find()
+            .populate("user", "name email")
+            .sort({ createdAt: -1 });
+        res.json({
+            success: true,
+            data: withdrawals
+        });
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
--- routes/withdrawal.routes.js
+++ routes/withdrawal.routes.js
@@ -3,12 +3,16 @@ const router = express.Router();
 
 const {
     createWithdrawal,
-    getMyWithdrawals
+    getMyWithdrawals,
+    getAllWithdrawals,
+    updateWithdrawalStatus
 } = require("../controllers/withdrawal.controller");
 
 const authMiddleware = require("../middleware/auth.middleware");
 
 router.post("/request", authMiddleware, createWithdrawal);
 router.get("/my", authMiddleware, getMyWithdrawals);
+router.get("/all", authMiddleware, getAllWithdrawals);
+router.put("/update-status/:id", authMiddleware, updateWithdrawalStatus);
 
 module.exports = router;
